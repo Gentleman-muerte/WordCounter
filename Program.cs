5cdf3469b5c081eb97a44378b7f1e7fd755c33c4
mathias <trolden08@hotmail.com>
2026-04-15 10:00:55 +0200

    opgave 5 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ProcessValue(25));
+        Console.WriteLine(ProcessValue("Hej verden"));
+        Console.WriteLine(ProcessValue(3.14));
+    }
+
+    public static dynamic ProcessValue(dynamic input)
+    {
+        if (input is int)
+        {
+            return 100 + input;
+        }
+
+        if (input is string)
+        {
+            return "Følgende tekst modtaget: " + input;
+        }
+
+        return false;
+    }
+}
